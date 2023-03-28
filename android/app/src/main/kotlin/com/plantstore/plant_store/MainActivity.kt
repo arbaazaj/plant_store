a0a03a1eaee991bc00d7a85827939f3f9c3a5b0f
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-03-28 08:00:28 +0530

    Initial commit.

---
+++ android/app/src/main/kotlin/com/plantstore/plant_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.plantstore.plant_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/themes/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color green = Color(0xFF347357);
